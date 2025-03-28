e796d5d333eadac4c95549b5a06f150259d75d4a
Fidel Cabañas <35480152+fideuardo@users.noreply.github.com>
2025-03-28 16:04:05 -0600

    The module has been adapted for IPCpy project
    * The tacometer was erased. This will developed another repository
    
    * The file was removed
    
    * __init__.py was added
    
    * unnecessary code was removed

--- Speedometer.py
+++ Speedometer.py
@@ -1,73 +1,71 @@
 import cv2
 import numpy as np
-from Lib.Graphics.AnalogGauge import AnalogGauge
+from .Lib.Graphics.AnalogGauge import AnalogGauge
 
 class Speedometer:
-    def __init__(self, height=500, width=800, type="analog"):
-        self.type = type
-        self.MaxSpeed = 180
-        self.MaxRPM = 8000
-        self.width = width
-        self.height = height
-        
-        #define the size (width,height) of the windows
-        self.GaugeSize = (300,300)
+    def __init__(self, image: np.ndarray, type="analog", MaxValue=240):
+        """
+        Initializes the Speedometer instance.
 
-        self.base_image = np.zeros((height, width, 3), dtype=np.uint8)
-        self.bg_color = (0, 0, 0)
-        self.base_image[:] = self.bg_color
+        Parameters:
+            image (np.ndarray): The main window image (background).
+            type (str): The type of speedometer ("analog" or other types).
+            MaxValue (int): The maximum speed value.
+            MinValue (int): The minimum speed value.
+        """
+        self.type = type
+        self.base_image = image
+        self.MaxSpeed = MaxValue
+        self.current_speed = 0
 
         if self.type == "analog":
             self.speed_gauge = AnalogGauge(
-                width = self.GaugeSize[0], height = self.GaugeSize[1], max_value=self.MaxSpeed, min_value= 0, minor_marks = 20, units='km/h',
-                arch=180, phase=180
-                )
-            self.tachometer_gauge = AnalogGauge(
-                width = self.GaugeSize[0], height = self.GaugeSize[1], max_value=self.MaxRPM, minor_marks = 2000, units='RPM',
-                arch=180, phase=180
-                )
+                image=self.base_image,
+                max_value=self.MaxSpeed,
+                min_value=0,
+                minor_marks=20,
+                units='km/h',
+                arch=180,
+                phase=180)
+            self.speed_gauge.needle_position_range = self.current_speed
         
-        self.speed_pos = (width//2 - 320, 50)
-        self.tacho_pos = (width//2 + 20, 50)
+
+    def draw(self):
+        """
+        Updates the display of the speedometer.
+
+        Returns:
+            np.ndarray: The updated base image with the speedometer overlayed.
+        """
+        # Draw the speedometer gauge
+        return self.speed_gauge.update_display()
+
         
-        cv2.namedWindow("Speedometer", cv2.WINDOW_NORMAL)
-        cv2.resizeWindow("Speedometer", self.width, self.height)
     
-    def update_display(self):
-        self.base_image[:] = self.bg_color
-        
-        speed_img = self.speed_gauge.update_display()
-        tacho_img = self.tachometer_gauge.update_display()
-        
-        x1, y1 = self.speed_pos
-        self.base_image[y1: y1 + self.GaugeSize[1], x1: x1 + self.GaugeSize[0]] = speed_img
-        
-        x2, y2 = self.tacho_pos
-        self.base_image[y2: y2 + self.GaugeSize[1], x2: x2 + self.GaugeSize[0]] = tacho_img
-        
-        return self.base_image
+    def set_speed(self, speed):
+        """
+        Updates the speed value.
+
+        Parameters:
+            speed (int): The new speed value.
+        """
+        if( self.current_speed != speed):
+            self.current_speed = speed
+
+        self.speed_gauge.update_value(speed)
+
 
 
 if __name__ == "__main__":
-    speedometer = Speedometer(height=500, width=800)
-    speed = 0
-    rpm = 0
-    while True:
-        speedometer.speed_gauge.needle_angle = speed
-        speedometer.tachometer_gauge.needle_position_range = rpm
+    # Define the size of the main window
+    window_size = (500, 800, 3)
+    base_image = np.zeros(window_size, dtype=np.uint8)
+    base_image[:, :] = (0, 0, 0)  # Black background
 
-        img = speedometer.update_display()
-        
-        cv2.imshow("Speedometer", img)
-        
-        key = cv2.waitKey(1)
-        if key == ord('q'):
-            break
-        elif key == ord('+'):
-            speed = speed +10
-            rpm = rpm + 1000
-        elif key == ord('-'):
-            speed = speed - 10
-            rpm = rpm - 1000
+    # Create the Speedometer instance
+    speedometer = Speedometer(image=base_image, type = "analog", MaxValue=240)
+    base_image = speedometer.draw()
 
+    cv2.imshow("speedometer", base_image)
+    cv2.waitKey()
     cv2.destroyAllWindows()
