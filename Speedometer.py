20f38bbb5e926bc9c2ba7df0eca940367f2b491b
Fidel Cabañas <35480152+fideuardo@users.noreply.github.com>
2025-04-04 19:10:36 -0600

    New updates were done by libraries updated (#8)
    * IPCGl module has been removed
    
    * The .gitignore file was updated
    
    * The Module is now pointing to IPCGLpy last version
    
    * Start the integration of new features
    
    * Graphic Libraries were updated
    
    * The code was updated according libraries updates

--- Speedometer.py
+++ Speedometer.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-from .Lib.Graphics.AnalogGauge import AnalogGauge
+from Lib.Graphics.AnalogGauge import AnalogGauge
 
 class Speedometer:
     def __init__(self, image: np.ndarray, type="analog", MaxValue=240):
@@ -27,8 +27,8 @@ class Speedometer:
                 units='km/h',
                 arch=180,
                 phase=180)
-            self.speed_gauge.needle_position_range = self.current_speed
-        
+            self.speed_gauge.SetValue(self.current_speed)
+        self.speed_gauge.update_display()
 
     def draw(self):
         """
@@ -37,8 +37,7 @@ class Speedometer:
         Returns:
             np.ndarray: The updated base image with the speedometer overlayed.
         """
-        # Draw the speedometer gauge
-        return self.speed_gauge.update_display()
+        self.speed_gauge.update_display()
 
         
     
@@ -51,21 +50,72 @@ class Speedometer:
         """
         if( self.current_speed != speed):
             self.current_speed = speed
-
-        self.speed_gauge.update_value(speed)
+            self.speed_gauge.SetValue(speed) 
+            self.speed_gauge.update_display()
 
 
 
 if __name__ == "__main__":
     # Define the size of the main window
+    imagename = 'speedometer'
     window_size = (500, 800, 3)
-    base_image = np.zeros(window_size, dtype=np.uint8)
-    base_image[:, :] = (0, 0, 0)  # Black background
+    MaxSpeed = 240
+    MinSpeed = 0
+    speed = 0
+    imagecontainer= np.zeros(window_size, dtype=np.uint8)
+    imagecontainer[:, :] = (0, 0, 0)  # Black background
 
     # Create the Speedometer instance
-    speedometer = Speedometer(image=base_image, type = "analog", MaxValue=240)
-    base_image = speedometer.draw()
+    speedometer = Speedometer(image=imagecontainer, type = "analog", MaxValue = MaxSpeed)
+    # Create window image
+    cv2.namedWindow(imagename)
+    cv2.imshow(imagename, imagecontainer)
 
-    cv2.imshow("speedometer", base_image)
-    cv2.waitKey()
+    while True:
+        # Display the updated image in the window
+        #cv2.imshow(imagename, imagecontainer)
+        # Wait for a key press
+        # 0xFF is used to mask the key value to get the last 8 bits
+        key = cv2.waitKey(1) & 0xFF
+
+        # Exit if 'q' is pressed
+        if key == ord('q'):
+            break
+        if key == ord('a'):
+            # set speedometer speed value to 0
+            speed = int(0)
+
+        if key == ord('b'):
+            # Update speedometer speed value to 25
+            speed = int((MaxSpeed * 0.25))
+
+        if key == ord('c'):
+            # Update speedometer speed value
+            speed = int((MaxSpeed * 0.50))
+
+        if key == ord('d'):
+            # Update speedometer speed value
+            speed = int((MaxSpeed * 0.75))
+
+        if key == ord('e'):
+            # Update speedometer speed value
+            speed = int(MaxSpeed)
+
+        if key == ord('+'):
+            # Update speedometer speed value
+            speed = speed + 5
+            if speed > MaxSpeed:
+                speed = MaxSpeed
+
+        if key == ord('-'):
+            # Update speedometer speed value
+            speed = speed - 5
+            if speed < MinSpeed:
+                speed = MinSpeed
+        
+        # Update speedometer speed value    
+        speedometer.set_speed(speed)
+        cv2.imshow(imagename, imagecontainer)
+
+    
     cv2.destroyAllWindows()
